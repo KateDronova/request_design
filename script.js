bb116ad234668392dc4f55430aae76473563c051
Ekaterina Dronova <katedronova96@gmail.com>
2023-11-10 03:02:02 +0300

    feat: add application markup and validation

--- script.js
+++ script.js
@@ -23,7 +23,11 @@ fetch('./productCardsInfo.json')
   .then((data) =>
     data.map((item) => {
       const catalog = document.querySelector('.catalog');
-      const card = createElementWithClass(catalog, 'div', 'catalog__productCard');
+      const card = createElementWithClass(
+        catalog,
+        'div',
+        'catalog__productCard'
+      );
 
       createElementWithClass(card, 'p', 'catalog__index', item.index);
       const image = createElementWithClass(card, 'img', 'catalog__img');
@@ -39,13 +43,31 @@ fetch('./newsData.json')
       const container = document.querySelector('.news__main');
       const card = createElementWithClass(container, 'div', 'news__card');
 
-      createElementWithClass(card, 'img', 'news__img').setAttribute('src', item.imgSrc);
-      createElementWithClass(card, 'img', 'news__supportingImg').setAttribute('src', './assets/images/supporting1.png');
-      createElementWithClass(card, 'img', 'news__supportingImg2').setAttribute('src', './assets/images/supporting2.png');
-      createElementWithClass(card, 'p', 'h3', item.title).classList.add('news__title');
+      createElementWithClass(card, 'img', 'news__img').setAttribute(
+        'src',
+        item.imgSrc
+      );
+      createElementWithClass(card, 'img', 'news__supportingImg').setAttribute(
+        'src',
+        './assets/images/supporting1.png'
+      );
+      createElementWithClass(card, 'img', 'news__supportingImg2').setAttribute(
+        'src',
+        './assets/images/supporting2.png'
+      );
+      createElementWithClass(card, 'p', 'h3', item.title).classList.add(
+        'news__title'
+      );
       createElementWithClass(card, 'p', 'news__decription', item.description);
+
       const button = createElementWithClass(card, 'button', 'news__readMore');
-      createElementWithClass(button, 'img').setAttribute('src', './assets/icons/→.svg');
-      createElementWithClass(card, 'div', 'news__date', item.day);
+      createElementWithClass(button, 'img').setAttribute(
+        'src',
+        './assets/icons/→.svg'
+      );
+
+      const dateContainer = createElementWithClass(card, 'div', 'news__date');
+      createElementWithClass(dateContainer, 'p', 'news__dateDay', item.day);
+      createElementWithClass(dateContainer, 'p', 'news__dateMonth', item.month);
     })
   );
