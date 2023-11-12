c4a83354e4cc2e33f77a1fbc86fe227eee8aab47
Ekaterina Dronova <katedronova96@gmail.com>
2023-11-12 23:47:49 +0300

    feat: add responsiveness for 2 sections

--- scripts/cards.js
+++ scripts/cards.js
@@ -37,28 +37,64 @@ fetch('./newsData.json')
       const container = document.querySelector('.news__main');
       const card = createElementWithClass(container, 'div', 'news__card');
 
-      createElementWithClass(card, 'div', 'news__img').style.backgroundImage = `url(${item.imgSrc})`;
-      createElementWithClass(card, 'img', 'news__supportingImg').setAttribute(
-        'src',
-        './assets/images/supporting1.png'
-      );
-      createElementWithClass(card, 'img', 'news__supportingImg2').setAttribute(
-        'src',
-        './assets/images/supporting2.png'
-      );
+      const dateContainer = createElementWithClass(card, 'div', 'news__date');
+      createElementWithClass(dateContainer, 'p', 'news__dateDay', item.day);
+
+      createElementWithClass(
+        card,
+        'div',
+        'news__img'
+      ).style.backgroundImage = `url(${item.imgSrc})`;
       createElementWithClass(card, 'p', 'h3', item.title).classList.add(
         'news__title'
       );
-      createElementWithClass(card, 'p', 'news__decription', item.description);
 
-      const button = createElementWithClass(card, 'button', 'news__readMore');
-      createElementWithClass(button, 'img').setAttribute(
-        'src',
-        './assets/icons/→.svg'
-      );
+      if (document.body.clientWidth > 425) {
+        createElementWithClass(
+          dateContainer,
+          'p',
+          'news__dateMonth',
+          item.month
+        );
 
-      const dateContainer = createElementWithClass(card, 'div', 'news__date');
-      createElementWithClass(dateContainer, 'p', 'news__dateDay', item.day);
-      createElementWithClass(dateContainer, 'p', 'news__dateMonth', item.month);
+        createElementWithClass(card, 'img', 'news__supportingImg').setAttribute(
+          'src',
+          './assets/images/supporting1.png'
+        );
+        createElementWithClass(
+          card,
+          'img',
+          'news__supportingImg2'
+        ).setAttribute('src', './assets/images/supporting2.png');
+
+        let newDescription = item.description.split(' ');
+        newDescription.length = 6;
+        newDescription = newDescription.join(' ');
+        createElementWithClass(card, 'p', 'news__decription', newDescription);
+
+        const button = createElementWithClass(card, 'button', 'news__readMore');
+        createElementWithClass(button, 'img').setAttribute(
+          'src',
+          './assets/icons/→.svg'
+        );
+      } else {
+        createElementWithClass(
+          dateContainer,
+          'p',
+          'news__dateMonth',
+          item.month.slice(0, 3)
+        );
+
+        createElementWithClass(card, 'img', 'news__supportingImg').setAttribute(
+          'src',
+          './assets/images/supporting1-375.png'
+        );
+        createElementWithClass(card, 'img', 'news__readMore375').setAttribute(
+          'src',
+          './assets/icons/button-black.png'
+        );
+
+        createElementWithClass(card, 'p', 'news__decription', item.description);
+      }
     });
   });
